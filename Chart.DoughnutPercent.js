0f66f5be44e941b091c4b058540526667052ae47
Andy Soell <andy@teamsoell.com>
2015-08-08 23:23:35 -0400

    Added exterior stroke

--- Chart.DoughnutPercent.js
+++ Chart.DoughnutPercent.js
@@ -13,5 +13,16 @@ Chart.types.Doughnut.extend({
   draw: function () {
     // Apply prototype rendering
     Chart.types.Doughnut.prototype.draw.apply(this, arguments);
-   }
+    // Add the exterior stroke
+    var centerX = this.chart.width / 2;
+    var centerY = this.chart.height / 2;
+    var radius = this.chart.height / 2;
+
+    this.chart.ctx.beginPath();
+    this.chart.ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    this.chart.ctx.lineWidth = 1;
+    this.chart.ctx.strokeStyle = this.options.strokeColor;
+    this.chart.ctx.stroke();
+
+  }
 });
